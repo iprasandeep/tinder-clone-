0a4970c93cfda33581056109d865e473f17a517f
prsndp <prasandeepp@gmail.com>
2022-12-30 10:11:17 +0530

    project structure created!

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import Header from './components/Header/Header';
+
+function App() {
+  return (
+    <div className="App">
+      {/* DBM class naming convention */}
+     <h1> Lets Load th MERN Tinder!</h1>
+      <Header />
+     {/* Tinder Cards */}
+    {/* SwipeButtons */}
+    
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './header.css';
+import PersonIcon from '@mui/icons-material/Person'; 
+const Header = () => {
+  return (
+    <div>
+      <h2>I am from Header!</h2>
+       < PersonIcon />
+    </div>
+  )
+}
+export default Header;
